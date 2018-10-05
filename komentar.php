ec6378972a4215751d8287187ec30bfa0a92bd20
zuritzars <43513387+zuritzars@users.noreply.github.com>
2018-10-05 10:29:29 +0700

    Update komentar.php
    6701174056_ZURITZA RAMADEAN SUHARMA

--- komentar.php
+++ komentar.php
@@ -1,3 +1,25 @@
+<form method="POST">
+	<table>
+		<tr>
+			<td>CERPEN</td>
+			<td>:</td>
+			<td><textarea name="cerpen"></textarea></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td><input type="submit" name="submit" value="Masukkan"></td>
+		</tr>
+	</table>
+</form>
 <?php
+	if (isset($_POST['submit'])) {
+		$cerpen=$_POST['cerpen'];
+		if(str_word_count($cerpen)>=5==TRUE){
+			echo "$cerpen";
+		}else{
+			echo "Huruf harus lebih atau sama dengan dari 5";
+		}
+	}
 
 ?>
