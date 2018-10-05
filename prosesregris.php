aa43662d2acfac1508d35a8252e9fce1e8e1afd3
zuritzars <43513387+zuritzars@users.noreply.github.com>
2018-10-05 10:12:03 +0700

    Update prosesregris.php
    6701174056_ZURITZA RAMADEAN SUHARMA

--- prosesregris.php
+++ prosesregris.php
@@ -1,4 +1,30 @@
+<form method="POST">
+	<table>
+		<tr>
+			<td>Nama</td>
+			<td>:</td>
+			<td><input type="text" name="nama"></td>
+		</tr>
+		<tr>
+			<td>NIM</td>
+			<td>:</td>
+			<td><input type="text" name="nim"></td>
+		</tr>
+		<tr>
+			<td>E-mail</td>
+			<td>:</td>
+			<td><input type="email" name="email"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td><input type="submit" name="submit" value="DAFTAR"></td>
+		</tr>
+	</table>
+</form>
 <?php
-
+	if (isset($_POST['submit'])) {
+		include 'regristrasi.php';
+	}
 
 ?>
