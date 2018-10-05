23c853ec11218279dbe86706751f994538f75a0a
zuritzars <43513387+zuritzars@users.noreply.github.com>
2018-10-05 10:12:56 +0700

    Update regristrasi.php
    6701174056_ZURITZA RAMADEAN SUHARMA

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,34 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
+<?php
+$koneksi = new mysqli("localhost", "root", "", "mahasiswa");
+	if (isset($_POST['submit'])) {
+		$koneksi = new mysqli("localhost", "root", "", "mahasiswa");
+		if ($koneksi) {
+			$nim = $_POST['nim'];
+			$nama = $_POST['nama'];
+			$email = $_POST['email'];
+			$row =mysqli_fetch_array(mysqli_query($koneksi,"SELECT*FROM daftar WHERE nim =$nim"));
 
-
-
-</body>
-</html>
+				if(strlen($nim)==10 && is_numeric($nim)==TRUE){
+					if(strlen($nama)<=25 && !preg_match("/^[a-zA-Z]*$/",$nama)==TRUE){
+						if(strpos($email, '@gmail.com')==TRUE || strpos($email, 'yahoo.com')==TRUE ){  
+						$sql = $koneksi->query("
+						INSERT INTO `daftar` (`nim`, `nama`, `email`)
+						VALUES ('$nim', '$nama','$email')
+										   ");
+						echo "Pendaftaran Berhasil<br>";
+						}else{
+							echo "NIM sudah digunakan mahasiswa lain!";
+							}
+						}else{
+							echo "Nama yang dimasukkan minimal 25 karakter! Dan hanya huruf saja!";
+						}
+					}else{
+						echo "Email harus sesuai aturan gmail atau Yahoo!";
+						}
+		}else{
+			echo "NIM harus 10 karakter dan hanya angka!";
+			}
+	}else{
+		echo "Pendaftaran GAGAL!";
+		}
+ ?>
